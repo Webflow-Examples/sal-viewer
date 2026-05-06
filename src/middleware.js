ee95304359d136ead213d7f8605424249dfc93f4
Raymond Camden <raymondcamden@gmail.com>
2026-05-06 11:21:23 -0500

    pp

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,11 @@ export async function onRequest (context, next) {
 
     console.log(context.url.pathname);
 
-    if(context.url.pathname.endsWith('/') || context.url.pathname.endsWith('/callback') || context.url.pathname.endsWith('/index.html')) return next();
+    if(context.url.pathname.endsWith('/') 
+        || context.url.pathname.endsWith('/callback') 
+        || context.url.pathname.endsWith('/index.html')
+        || context.url.pathname.endsWith('/privacy.html')
+    ) return next();
 
     console.log('need to check stuff', context.url.pathname);
     let token = await context.session.get('access_token');
